df0632111215f6f527c8e7110998b5207262858c
Sudip-Naskar <sudipdev09@gmail.com>
2023-08-31 12:11:06 +0530

    CSS added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given")
 const checkButton = document.querySelector("#check-button")
 const message = document.querySelector("#error-message")
+const noOfNotes = document.querySelectorAll(".no-of-notes")
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
@@ -17,8 +20,19 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
     }
 })
 
-function calculateChange() {
-    
+function calculateChange(amountToBeReturned) {
+    // go over all the available notes
+    for(let i = 0; i < availableNotes.length; i++){
+
+        // no of notes needed for the denomination
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i])
+
+        // amount left after calculating the number of notes needed
+        amountToBeReturned = amountToBeReturned % availableNotes[i]
+        
+        // updating the no of notes in the table for the current amount
+        noOfNotes[i].innerText = numberOfNotes;
+    }
 }
 
 function hideMessage(){
